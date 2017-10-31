841a0a11c4abc3234880d9b570b5fdb325f12f64
midnight <midnight@localhost.localdomain>
2017-10-30 21:54:59 -0600

    actualizar index

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    
+    <title>Cooperativa POO</title>
+</head>
+<body>
+    <script src="js/index.js"></script>
+</body>
+</html>
